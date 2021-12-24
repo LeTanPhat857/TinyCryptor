5fd984994c91b45a93c30b8b2e493b0359845aa4
LeTanPhat <letanphat857@gmail.com>
2021-12-24 12:14:27 +0700

    edit test

--- src/test/java/library/TestBouncyCastleLibrary.java
+++ src/test/java/library/TestBouncyCastleLibrary.java
@@ -1,23 +1,30 @@
-package test;
+package library;
 
-import org.junit.jupiter.api.DisplayName;
-
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.Test;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.Security;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
 
-public class Test {
+public class TestBouncyCastleLibrary {
 
-    @DisplayName("test")
-    @org.junit.jupiter.api.Test
-    public void test() {
-        listAlgorithm("Cipher.PBEWith");
+    @Test
+    public void addProvider() {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+            Cipher.getInstance("Noekeon");
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
     }
 
-    public List<String> listAlgorithm(String algorithmType) {
-        List<String> result = new ArrayList<>();
+    @Test
+    public void listAlgorithm() {
+        Security.addProvider(new BouncyCastleProvider());
+        String algorithmType = "Cipher.PBEWith";
         try {
             Provider[] providers = Security.getProviders();
             for (Provider provider : providers) {
@@ -32,6 +39,5 @@ public class Test {
         } catch (Exception e) {
             System.out.println(e);
         }
-        return result;
     }
 }
